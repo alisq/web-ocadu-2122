178544a00a89014bab3403b3ef59c62ef765f76c
ame-bui <66383380+ame-bui@users.noreply.github.com>
2021-11-13 10:16:19 -0500

    1113 - 10:15

--- 02-kubrick/script/faq-script.js
+++ 02-kubrick/script/faq-script.js
@@ -1,2 +1,13 @@
 
 
+$(".index-tag").click(function(){
+    let indexId = $(this).attr("id");
+    $(".sub-nav-wrapper #"+indexId).toggle();
+})
+
+$(".tag-button").click(function(){
+    let buttonId = $(this).attr("id");
+    console.log(buttonId);
+
+    $(".question-tags ."+buttonId).toggle();
+})
